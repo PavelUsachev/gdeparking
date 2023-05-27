fdb4172cab6f0554ef05607bcd02c38088385b68
Konstantin <prohogiy69@gmail.com>
2023-05-27 21:57:32 +0300

    car_detection.py changed
    1) Changed the output of logs, now the logs are displayed without delay
    2) Added processing of exceptions. Set to restart a failed thread with a delay of 10 seconds
    3) Added deletion of video files after the end of the CV model processing  instead of overwriting the previous video.

--- CV/car_detect.py
+++ CV/car_detect.py
@@ -24,6 +24,7 @@ def driver_init():
 
     options.add_argument("--no-sandbox")
     options.add_argument("--headless")
+    options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument("start-maximized")
     options.add_argument("--autoplay-policy=no-user-gesture-required")
@@ -82,8 +83,9 @@ def park_place_detection(driver_, cam_url, camera_id, net, park_zones, t_sleep):
     pred_data = []
     connect_time_status = get_video(driver_, cam_url, camera_id, t_sleep)
     if connect_time_status is not None:
+        video_file = f"./temp/tmp_{camera_id}.mp4"
         try:
-            clip = VideoFileClip(f"./temp/tmp_{camera_id}.mp4")
+            clip = VideoFileClip(video_file)
             last_frame = clip.get_frame(clip.duration - 1)[:, :, ::-1]
             for zone in park_zones:
                 x, y, w, h = zone["x"], zone["y"], zone["w"], zone["h"]
@@ -95,69 +97,83 @@ def park_place_detection(driver_, cam_url, camera_id, net, park_zones, t_sleep):
                 pred_data.append((zone["name"], int(predict)))
         except:
             connect_time_status = None
+
+    if os.path.exists(video_file):
+        os.remove(video_file)
     return pred_data, connect_time_status
 
 
 def one_cam_threading(cam_nb, server_address):
+    web_driver = None
 
-    with open(f"./devices_metadata/{cam_nb}_metadata.txt", "r") as f:
-        data = f.read()
-    metadata = json.loads(data)
+    while True:
+        try:
+            with open(f"./devices_metadata/{cam_nb}_metadata.txt", "r") as f:
+                data = f.read()
+            metadata = json.loads(data)
 
-    detection = True
-    last_prediction = None
-    last_connection_time = None
+            detection = True
+            last_prediction = None
+            last_connection_time = None
 
-    url = metadata["cam_url"]
-    zones = metadata["detect_zones"]
-    sleep_time = metadata["update_period"] // 2 + 1
+            url = metadata["cam_url"]
+            zones = metadata["detect_zones"]
+            sleep_time = metadata["update_period"] // 2 + 1
 
-    model_path = "./cv_model/parking_detect.onnx"
-    model = cv2.dnn.readNetFromONNX(model_path)
+            model_path = "./cv_model/parking_detect.onnx"
+            model = cv2.dnn.readNetFromONNX(model_path)
 
-    web_driver = driver_init()
+            web_driver = driver_init()
 
-    while detection:
-        prediction, connection_time = park_place_detection(web_driver, url, cam_nb, model, zones, sleep_time)
+            while detection:
+                prediction, connection_time = park_place_detection(web_driver, url, cam_nb, model, zones, sleep_time)
 
-        if last_connection_time is None and connection_time is not None:
-            print(f"Connected to the camera {cam_nb.split('_')[-1]} successfully!")
-            last_prediction = dict(prediction)
-            last_connection_time = connection_time
-        elif connection_time is None:
-            print(f"Camera {cam_nb.split('_')[-1]} connection error! The next attempt will be made in a few seconds...")
-        else:
-            last_prediction = dict(prediction)
-            last_connection_time = connection_time
+                if last_connection_time is None and connection_time is not None:
+                    print(f"Connected to the camera {cam_nb.split('_')[-1]} successfully!", flush=True)
+                    last_prediction = dict(prediction)
+                    last_connection_time = connection_time
+                elif connection_time is None:
+                    print(f"Camera {cam_nb.split('_')[-1]} connection error! The next attempt will be made in a few "
+                          f"seconds...", flush=True)
+                else:
+                    last_prediction = dict(prediction)
+                    last_connection_time = connection_time
 
-        detection_result = {"detection_result": last_prediction,
-                            "last_connection": last_connection_time,
-                            "metadata": {"cam_id": cam_nb,
-                                         "cam_address": metadata["cam_address"],
-                                         "park_places_nb": len(zones),
-                                         "timezone": metadata["timezone"],
-                                         "update_period": metadata["update_period"]}}
-        if server_address is not None:
-            package = requests.post(server_address, json=detection_result)
-            if package.status_code != 200:
-                print(f"Failed to send {cam_nb} data package!")
-        else:
-            print(f"Camera {cam_nb.split('_')[-1]}:\nlast_connection: {last_connection_time}\ndetection_result:"
-                  f" {last_prediction}")
+                detection_result = {"detection_result": last_prediction,
+                                    "last_connection": last_connection_time,
+                                    "metadata": {"cam_id": cam_nb,
+                                                "cam_address": metadata["cam_address"],
+                                                "park_places_nb": len(zones),
+                                                "timezone": metadata["timezone"],
+                                                "update_period": metadata["update_period"]}}
+                if server_address is not None:
+                    package = requests.post(server_address, json=detection_result)
+                    if package.status_code != 200:
+                        print(f"Failed to send {cam_nb} data package!", flush=True)
+                else:
+                    print(f"Camera {cam_nb.split('_')[-1]}:\nlast_connection: {last_connection_time}\ndetection_result:"
+                          f"{last_prediction}", flush=True)
+        except Exception as e:
+            print(f"An error occurred in thread-{cam_nb.split('_')[-1].split('0')[-1]}: {e}. Restarting the thread...",
+                  flush=True)
+            if web_driver is not None:
+                web_driver.quit()
+            time.sleep(10)
 
-    web_driver.quit()
+    if web_driver is not None:
+        web_driver.quit()
 
 
 if __name__ == "__main__":
 
-    print("Service starts...")
+    print("Service starts...", flush=True)
 
     load_dotenv()
     api_url = os.getenv("API_URL")
 
     if api_url is None:
         print(f"No server address for send detection results! Check the .env file in the project directory. The "
-              f"results of the detection will be redirected to standard output.")
+              f"results of the detection will be redirected to standard output.", flush=True)
 
     threads = []
     for _, _, files in os.walk("./devices_metadata"):
@@ -166,7 +182,7 @@ if __name__ == "__main__":
             t = threading.Thread(target=one_cam_threading, args=(cam_id, api_url,))
             t.start()
             threads.append(t)
-            print(f"Thread-{cam_id.split('_')[-1].split('0')[-1]} started.")
+            print(f"Thread-{cam_id.split('_')[-1].split('0')[-1]} started.", flush=True)
 
     for t in threads:
         t.join()
